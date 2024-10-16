af3a4c0e8903e5075ddf9fae8efbf26505d46cbc
Gabi <gabrielle.sote@gmail.com>
2024-10-16 15:57:38 -0300

    Colocando todas as atualizações que fiz na version 1 (não irei detalhar porque já detalhei no que deu errado, me perguta qualquer coisa)

--- loja/src/app/components/login/login.component.ts
+++ loja/src/app/components/login/login.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // Variável para controlar qual formulário será exibido
+  isLogin: boolean = true;
 
+  // Alternar entre login e registro
+  toggleForm() {
+    this.isLogin = !this.isLogin;
+  }
 }
